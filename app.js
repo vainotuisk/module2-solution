8e4641ec865bdfe5907d6cfff2027060b384ba27
vainotuisk <vainotuisk@gmail.com>
2016-10-02 12:03:24 +0300

    tobuylist on ok

--- app.js
+++ app.js
@@ -25,27 +25,47 @@ angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+// Controller for bought items
+
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
 var boughtController = this;
 boughtController.boughtlist = ShoppingListCheckOffService.getBoughtlist();
-}
+};
 
-
+// Controller for tobuy items
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
+var buyController = this;
+buyController.buylist = ShoppingListCheckOffService.getbuyList();
+console.log('otsitav:' + buyController.buylist );
+buyController.buyItem = function(itemIdex) {
+  ShoppingListCheckOffService.buyItem(itemIdex);
 
 }
+};
+
+// service for both controllers
 
 function ShoppingListCheckOffService() {
   var service = this;
 
   // List of shopping items
   var tobuyList = initialList;
+
   var boughtList = [];
   service.getbuyList = function () {
+    console.log(tobuyList);
     return tobuyList;
-  }
+  };
+
+  service.getBoughtlist = function () {
+    return boughtList;
+  };
+  service.buyItem = function(itemIdex) {
+    boughtList.push(tobuyList[itemIdex]);
+    tobuyList.splice(itemIdex,1);
+  };
 
   service.addItem = function (itemName, quantity) {
     var item = {
